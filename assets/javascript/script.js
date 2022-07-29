f2fa0b1a582951dd1411de03bb3a8c43d526d18e
Arthur <odthientho@gmail.com>
2022-07-28 21:10:40 -0400

    Highscores Records done. Timer done.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -56,14 +56,94 @@ var question = document.querySelector("#question");
 var startBtn = document.querySelector("#start-Btn");
 var result = document.querySelector("#result");
 var timer = document.querySelector("#timer");
+var recordScores = document.querySelector("#recordScores-form");
+var initials = document.querySelector("#initials");
+var scoreSpan = document.querySelector("#score");
+var highscoresList = document.querySelector("#highscoresList");
+var viewHighscores = document.querySelector("#viewHighscores");
+var goBackBtn = document.querySelector("#goBack");
+var clearHighScoresBtn = document.querySelector("#clearHighScores");
 
 var questionNum;
 var timeOut;
+var maximumTime = 60;
+var score;
+var highscoreRecords;
+var isLast;
+
+function visibilitySwitch(className, isVisible) {
+    var tmp = document.querySelector("."+className);
+    tmp.setAttribute("style", "visibility: "+isVisible);
+}
+
+function showHighScores() {
+    highscoresList.innerHTML = "";
+
+    highscoreRecords = [];
+    var temp = JSON.parse(localStorage.getItem("highscoreRecords"));
+    if (temp != null) highscoreRecords = temp;
+
+    if (highscoreRecords.length == 0) {
+        var li = document.createElement("li");
+        li.textContent = "";
+        highscoresList.appendChild(li);   
+    } else {
+        for (var i=0; i < highscoreRecords.length; i++) {
+            var li = document.createElement("li");
+            li.textContent = highscoreRecords[i].initials+" - "+highscoreRecords[i].score+" corrects - "+highscoreRecords[i].seconds+" sec";
+            highscoresList.appendChild(li);       
+        }
+    }
+    
+    visibilitySwitch("introduction", "hidden");
+    visibilitySwitch("questions", "hidden");
+    visibilitySwitch("correction", "hidden");
+    visibilitySwitch("records", "hidden");
+    visibilitySwitch("highscores", "visible");
+}
+
+viewHighscores.addEventListener("click", function(event) {
+    event.preventDefault();
+    showHighScores();
+});
+
+goBackBtn.addEventListener("click", function(event) {
+    init();
+});
+
+clearHighScoresBtn.addEventListener("click", function(event) {
+    localStorage.clear();
+    showHighScores();
+});
+
+recordScores.addEventListener("submit", function(event){
+    event.preventDefault();
+
+    var nameIni = initials.value;
+    var record = { 
+        initials: nameIni,
+        score: score,
+        seconds: maximumTime-timeOut
+    }
+    highscoreRecords = [];
+    var temp = JSON.parse(localStorage.getItem("highscoreRecords"));
+    if (temp != null) highscoreRecords = temp;
+    highscoreRecords.push(record);
+    localStorage.setItem("highscoreRecords",JSON.stringify(highscoreRecords));
+    initials.value = "";
+
+    showHighScores();
+
+});
 
 function nextQuestion() {
     if (questionNum > questionList.length) return;
     if (questionNum == questionList.length) {
-        
+        scoreSpan.textContent = "Your final score is "+score+" in "+(maximumTime-timeOut+1)+" seconds.";
+        visibilitySwitch("questions", "hidden");
+        visibilitySwitch("records", "visible");
+        isLast = true;
+        return;
     }
 
     question.textContent = questionList[questionNum].question;
@@ -72,31 +152,55 @@ function nextQuestion() {
     }
 }
 
-function visibilitySwitch(className, isVisible) {
-    var tmp = document.querySelector("."+className);
-    tmp.setAttribute("style", "visibility: "+isVisible);
-}
-
-startBtn.addEventListener("click", function () {
-    nextQuestion();
-    visibilitySwitch("introduction", "hidden");
-    visibilitySwitch("questions", "visible");
-
+function setTimeOut() {
     timer.textContent = "TIME: " + timeOut;
     var timerInterval = setInterval(function() {
         timeOut--;
         timer.textContent = "TIME: " + timeOut;
-        if(timeOut === 0) {
+
+        if (isLast) {
             clearInterval(timerInterval);
         }
+
+        if(timeOut <= 0) {
+            timeOut = 0;
+            timer.textContent = "TIME: " + timeOut;
+            clearInterval(timerInterval);
+            visibilitySwitch("questions", "hidden");
+            visibilitySwitch("records", "visible");
+        }
     }, 1000);
+}
+
+startBtn.addEventListener("click", function () {
+    questionNum = 0;
+    timeOut = maximumTime;
+    score = 0;
+    isLast = false;
+    
+    nextQuestion();
+    setTimeOut();
+
+    visibilitySwitch("introduction", "hidden");
+    visibilitySwitch("questions", "visible");
+
 });
 
 function checkAnswer(answerPicked) {
     if (questionNum >= questionList.length) return;
 
-    if (answerPicked != questionList[questionNum].correct) result.textContent = "Wrong!";
-    else result.textContent = "Correct!";
+    if (answerPicked != questionList[questionNum].correct) {
+        result.textContent = "Wrong!";
+        timeOut -= 5;
+        if(timeOut <= 0) {
+            clearInterval(timerInterval);
+            visibilitySwitch("questions", "hidden");
+            visibilitySwitch("records", "visible");
+        }
+    } else {
+        score++;
+        result.textContent = "Correct!";
+    }
 
     var onesecond = 5;
     var timerInterval = setInterval(function() {
@@ -109,12 +213,15 @@ function checkAnswer(answerPicked) {
     }, 300);
 
     questionNum++;
-    nextQuestion()
+    nextQuestion();
 }
 
 function init() {
-    questionNum = 0;
-    timeOut = 60;
+    visibilitySwitch("introduction", "visible");
+    visibilitySwitch("questions", "hidden");
+    visibilitySwitch("correction", "hidden");
+    visibilitySwitch("records", "hidden");
+    visibilitySwitch("highscores", "hidden");
 }
 
 init();
